0a319f8c4b0798b2b4e3c66486038b06ee5f3ebc
Arnav Dalmia <arnavd.me@gmail.com>
2024-04-03 11:05:31 -0400

    Initial Commit

---
+++ src/main/java/me/arnavdal/infoEntertainment/InfoEntertainmentApplication.java
@@ -0,0 +1,14 @@
+package me.arnavdal.infoEntertainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class InfoEntertainmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoEntertainmentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/me/arnavdal/infoEntertainment/ReviewRepository.java
@@ -0,0 +1,10 @@
+package me.arnavdal.infoEntertainment;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
